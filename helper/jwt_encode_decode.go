2e01b6a11b96cff10b4bb30ba2e928aca6cc8575
remon <blackshadowsoftwareltd@gmail.com>
2022-06-02 01:25:30 +0600

    jwt decode

--- helper/jwt_encode_decode.go
+++ helper/jwt_encode_decode.go
@@ -30,3 +30,20 @@ func GenerateJWTToken(id string) (string, error) {
 
 	return token, nil
 }
+
+///? Decode
+func DecriptJWTTokenToId(tokenString string) (string, string, error) {
+	fmt.Println("Decript Token")
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(data.SecretKey), nil //? []byte(SecretKey)
+	})
+
+	if err != nil {
+		fmt.Println("err ", err)
+		return "", "", err
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+	return claims.Id, claims.Issuer, nil
+}
